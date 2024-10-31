8e0836b81a7a97e074af1b9bcd43ad51c9a96a77
krish pandey <143093900+krish858@users.noreply.github.com>
2024-10-31 23:55:50 +0530

    Update Notfound.tsx

--- frontend/src/pages/Notfound.tsx
+++ frontend/src/pages/Notfound.tsx
@@ -8,7 +8,7 @@ export default function Notfound() {
     <div className="min-h-screen bg-gray-800 flex flex-col items-center justify-center text-white px-4">
       <AlertCircle className="w-24 h-24 text-[#28a745] mb-8" />
       <h1 className="text-4xl font-bold mb-4 text-center">
-        404 - Page Not Found
+        404 - Page Not Found.
       </h1>
       <p className="text-xl text-gray-400 mb-8 text-center max-w-md">
         Oops! It looks like you've wandered into uncharted territory. This page
